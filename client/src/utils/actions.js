7f51a79b12de48f2f47dfe8673149b9b980134a6
agvan <agvandyke25@gmail.com>
2022-09-20 13:06:39 -0500

    added actions and comments

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,2 +1,10 @@
 // actions define type of events that be emitted to update state
 // state only updated if predefined action
+
+// UPDATE_PRODUCTS used to store product data in global state instead of going through Apollo server every time
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// stores a list of categories in a global state
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// connects products and categories 
+export const UPDATE_CURRENT_CATEGORIES = "UPDATE_CURRENT_CATEGORY";
+
