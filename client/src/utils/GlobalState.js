d62779f08a9477cd36f571cf15f013879b7178e5
agvan <agvandyke25@gmail.com>
2022-09-20 14:44:13 -0500

    added StoreProvider

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -7,3 +7,14 @@ const StoreContext = createContext();
 // Provider is a special type of React component for wrapping the application
 // so that it can  make the state data that's passed into it as a prop available to all other components
 const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+      products: [],
+      categories: [],
+      currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+  };
