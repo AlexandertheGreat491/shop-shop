75a9742a6a26989f7209ac41e72e6ea9dcd12bdd
agvan <agvandyke25@gmail.com>
2022-09-20 14:31:39 -0500

    added  two comments

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,2 +1,4 @@
+// createContext is used to create new Context object
+// useContext is a React hook for using the state created by the createContext function
 import React, {createContext, useContext} from "react";
 import {useProductReducer} from './reducers';
