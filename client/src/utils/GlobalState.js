f3e4e8367d57ff5252b8d829199c841f4b15fa11
agvan <agvandyke25@gmail.com>
2022-09-20 14:33:38 -0500

    added two comments, fixed indentations, and added StoreContext variable and Provider object

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,4 +1,7 @@
 // createContext is used to create new Context object
 // useContext is a React hook for using the state created by the createContext function
-import React, {createContext, useContext} from "react";
-import {useProductReducer} from './reducers';
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from "./reducers";
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
