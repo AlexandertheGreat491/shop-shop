78a82f525485a7e48556fd327d8944fc3997f163
agvan <agvandyke25@gmail.com>
2022-09-20 13:40:51 -0500

    fixed indentations

--- client/src/_tests_/reducers.test.js
+++ client/src/_tests_/reducers.test.js
@@ -1,40 +1,40 @@
-import { reducer } from '../utils/reducers';
+import { reducer } from "../utils/reducers";
 // import our actions
 import {
-    UPDATE_PRODUCTS,
-    UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-  } from '../utils/actions';
-  
-  // create a sample of what our global state will look like
-  const initialState = {
-    products: [],
-    categories: [{ name: 'Food' }],
-    currentCategory: '1',
-  };
+  UPDATE_PRODUCTS,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from "../utils/actions";
 
-  // test for the UPDATE_PRODUCTS action
-  test('UPDATE_PRODUCTS', () => {
-    // newState will be the result of the reducer()
-    // accepts two arguments: the current state object & type & value
-    // value is not always called value & in this case is products
-    let newState = reducer(initialState, {
+// create a sample of what our global state will look like
+const initialState = {
+  products: [],
+  categories: [{ name: "Food" }],
+  currentCategory: "1",
+};
+
+// test for the UPDATE_PRODUCTS action
+test("UPDATE_PRODUCTS", () => {
+  // newState will be the result of the reducer()
+  // accepts two arguments: the current state object & type & value
+  // value is not always called value & in this case is products
+  let newState = reducer(initialState, {
     // type of action being performed
-      type: UPDATE_PRODUCTS,
-      products: [{}, {}]
-    });
-  
-    expect(newState.products.length).toBe(2);
-    expect(initialState.products.length).toBe(0);
+    type: UPDATE_PRODUCTS,
+    products: [{}, {}],
   });
 
-  // test for the UPDATE_CATEGORIES action
-  test('UPDATE_CATEGORIES', () => {
-    let newState = reducer(initialState, {
-      type: UPDATE_CATEGORIES,
-      categories: [{}, {}]
-    });
+  expect(newState.products.length).toBe(2);
+  expect(initialState.products.length).toBe(0);
+});
 
-    expect(newState.categories.length).toBe(2);
-    expect(initialState.categories.length).toBe(1);
+// test for the UPDATE_CATEGORIES action
+test("UPDATE_CATEGORIES", () => {
+  let newState = reducer(initialState, {
+    type: UPDATE_CATEGORIES,
+    categories: [{}, {}],
   });
+
+  expect(newState.categories.length).toBe(2);
+  expect(initialState.categories.length).toBe(1);
+});
