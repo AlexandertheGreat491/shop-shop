eb5785176012d403690c921e6aeacc69207b5a23
agvan <agvandyke25@gmail.com>
2022-09-20 14:51:25 -0500

    added useStoreContext function and exported StoreProvider and useStoreContext

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -20,3 +20,9 @@ const StoreProvider = ({ value = [], ...props }) => {
   console.log(state);
   return <Provider value={[state, dispatch]} {...props} />;
 };
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+  };
+
+  export { StoreProvider, useStoreContext };
