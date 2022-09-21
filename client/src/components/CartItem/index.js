afc0875f0ac3de6f18fb3ed102b0ce93945bfd18
agvan <agvandyke25@gmail.com>
2022-09-20 19:20:09 -0500

    imported useStoreContext and actions into CartItem

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 const CartItem = ({ item }) => {
   return (
