6bfa123e20d146d7d292943c48b17894d3e152fe
agvan <agvandyke25@gmail.com>
2022-09-20 20:30:57 -0500

    added the destructured dispatch function and the removeFromCart function

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
+const [, dispatch] = useStoreContext();
+
+const removeFromCart = item => {
+  dispatch({
+    type: REMOVE_FROM_CART,
+    id: item._id
+  });
+};
+
 const CartItem = ({ item }) => {
   return (
     <div className="flex-row">
